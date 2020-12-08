0f532f8e30673546edc4f592d8c05916fbcf7d59
Jane <m3281@hotmail.com>
2020-12-08 14:42:34 +0800

    add-new-list function

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,32 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function() {
+ 
+  // add new list
+  let addBtn = document.querySelector('.addBtn')
+  addBtn.addEventListener('click', function() {
+    
+    let newList = document.createElement('li')
+    let inputText = document.querySelector('#input').value
+    let finalText = document.createTextNode(inputText)
+    newList.appendChild(finalText)
+    document.querySelector('ul').appendChild(newList)
+    let span = document.createElement('span')
+    let cross= document.createTextNode('x')
+    span.className = 'close'
+    span.appendChild(cross)
+    newList.appendChild(span)
+
+
+    let close= document.querySelectorAll('.close')
+    for ( let i = 0; i < close.length; i++) {
+      close[i].addEventListener('click', function(t) {
+        if (t.target.className === 'close') {
+          let li = t.target.parentElement
+          li.remove()  
+        }     
+      })
+    } 
+  })
+
+
+})
