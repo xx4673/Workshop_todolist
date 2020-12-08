aa3beacf9ff8150f4c66cdd1a96c5dc606a689a3
Jane <m3281@hotmail.com>
2020-12-08 14:40:35 +0800

    add delete-function

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,15 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function() {
+
+  
+  // remove list
+  let close= document.querySelectorAll('.close')
+  for ( let i = 0; i < close.length; i++) {
+    close[i].addEventListener('click', function(t) {
+      if (t.target.className === 'close') {
+        let li = t.target.parentElement
+        li.remove()  
+      }     
+    })
+  }  
+})
