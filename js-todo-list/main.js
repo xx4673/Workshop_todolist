d486b16790102537eb80a20946452cf069220fef
Jane <m3281@hotmail.com>
2020-12-08 14:36:43 +0800

    add tick function

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,1 +1,11 @@
 // TO DO
+document.addEventListener('DOMContentLoaded', function() {
+
+  let lists = document.querySelector('ul')
+  // tick list
+  lists.addEventListener('click', function(t) {
+    if (t.target.tagName === 'LI') {
+      t.target.classList.toggle('checked')     
+    }     
+  })
+})
