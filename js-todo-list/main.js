c0f227e8ae03baa468f4ebfbc9978ea0393d17dd
Jane <m3281@hotmail.com>
2020-12-08 16:10:56 +0800

    review

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,41 +1,21 @@
 // TO DO
 document.addEventListener('DOMContentLoaded', function() {
-  let lists = document.querySelector('ul')
+  tickList()
+  removeList()
+
   // tick list
-  lists.addEventListener('click', function(t) {
-    if (t.target.tagName === 'LI') {
-      t.target.classList.toggle('checked')     
-    }     
-  })
-  
-  // remove list
-  let close= document.querySelectorAll('.close')
-  for ( let i = 0; i < close.length; i++) {
-    close[i].addEventListener('click', function(t) {
-      if (t.target.className === 'close') {
-        let li = t.target.parentElement
-        li.remove()  
+  function tickList() {
+    let lists = document.querySelector('ul')
+    lists.addEventListener('click', function(t) {
+      if (t.target.tagName === 'LI') {
+        t.target.classList.toggle('checked')     
       }     
     })
-  }  
-
- 
-  // add new list
-  let addBtn = document.querySelector('.addBtn')
-  addBtn.addEventListener('click', function() {
     
-    let newList = document.createElement('li')
-    let inputText = document.querySelector('#input').value
-    let finalText = document.createTextNode(inputText)
-    newList.appendChild(finalText)
-    document.querySelector('ul').appendChild(newList)
-    let span = document.createElement('span')
-    let cross= document.createTextNode('x')
-    span.className = 'close'
-    span.appendChild(cross)
-    newList.appendChild(span)
+  }
 
-
+  // remove list
+  function removeList() {
     let close= document.querySelectorAll('.close')
     for ( let i = 0; i < close.length; i++) {
       close[i].addEventListener('click', function(t) {
@@ -44,8 +24,36 @@ document.addEventListener('DOMContentLoaded', function() {
           li.remove()  
         }     
       })
-    } 
+    }  
+    
+  }
+
+  // add new list
+  let addBtn = document.querySelector('.addBtn')
+  addBtn.addEventListener('click', function() {
+    
+    let newList = document.createElement('li')
+    let inputText = document.querySelector('#input').value
+    let finalText = document.createTextNode(inputText)
+    
+    if (inputText != "") {     
+      newList.appendChild(finalText)
+      document.querySelector('ul').appendChild(newList)
+      let span = document.createElement('span')
+      let cross= document.createTextNode('x')
+      span.className = 'close'
+      span.appendChild(cross)
+      newList.appendChild(span)
+  
+      document.querySelector('#input').value = ""
+    }
+
+    removeList()
+
   })
 
+
+  // drag list
+  let listsItems = document.querySelectorAll('ul>li')
 
 })
